f11e1f5b8df876832cdfaed73f847b47fa7ce08c
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2025-08-11 20:38:10 +0200

    Added url variable to connect with renderDB

--- front/src/services/fetchUsers.ts
+++ front/src/services/fetchUsers.ts
@@ -2,11 +2,14 @@
 import IUsers from "../types/IUsers";
 
 const fetchUsers = async (data: IUsers): Promise<IUsers> => {
-  const response = await fetch("http://localhost:4001/users/register", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/register`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }
+  );
 
   if (!response.ok) throw new Error("Error, we couldn't registered you");
 
