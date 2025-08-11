a3444324c5f0a709adde24b993b00b2cc9a7674d
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2025-08-11 20:55:59 +0200

    Seed controler, preload just on time

--- back/src/seed.ts
+++ back/src/seed.ts
@@ -1,17 +1,26 @@
 import { preLoadCategories } from "./helpers/preLoadCategories";
 import { preLoadProducts } from "./helpers/preLoadProducts";
 import { AppDataSource } from "./config/dataSource";
+import { Category } from "./entities/Category";
+import { Product } from "./entities/Product";
 
 const seed = async () => {
   try {
-    // Initialize db
     await AppDataSource.initialize();
 
-    // Preload categories
-    await preLoadCategories();
+    const categoryCount = await AppDataSource.manager.count(Category);
+    if (categoryCount === 0) {
+      await preLoadCategories();
+    }
 
-    // Preload products
-    await preLoadProducts();
+    // Repite la lógica para los productos
+    const productCount = await AppDataSource.manager.count(Product);
+    if (productCount === 0) {
+      await preLoadProducts();
+    }
+
+    await AppDataSource.destroy();
+    process.exit(0);
   } catch (error) {
     process.exit(1);
   }
