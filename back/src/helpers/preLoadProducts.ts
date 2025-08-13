62e3ca99b3eae7e6607e7f20b01ec39421374a8d
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2025-08-13 15:29:57 +0200

    Categories updated

--- back/src/helpers/preLoadProducts.ts
+++ back/src/helpers/preLoadProducts.ts
@@ -19,7 +19,7 @@ const productsToPreLoad: IProduct[] = [
       "Experience power and elegance with the iPhone 11: capture stunning moments with its dual-camera system, enjoy exceptional performance, and immerse yourself in a brilliant Liquid Retina display. Discover a world of possibilities in the palm of your hand!",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/iphone11.jpg?updatedAt=1755005058145",
-    categoryId: 1,
+    categoryId: 10,
     stock: 10,
   },
   {
@@ -29,7 +29,7 @@ const productsToPreLoad: IProduct[] = [
       "Embrace efficiency and sophistication with the MacBook Air: lightweight design meets powerful performance, stunning Retina display brings your work to life, and all-day battery life keeps you productive wherever you go. Elevate your computing experience with the MacBook Air.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/macbookair.jpeg?updatedAt=1755005057791",
-    categoryId: 2,
+    categoryId: 11,
     stock: 10,
   },
   {
@@ -39,7 +39,7 @@ const productsToPreLoad: IProduct[] = [
       "Unleash your creativity and productivity with the iPad Pro: powerful performance, stunning Liquid Retina display, and all-day battery life make the iPad Pro the perfect tool for work and play. Transform your ideas into reality with the iPad Pro.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/ipadPro.webp?updatedAt=1755005057519",
-    categoryId: 3,
+    categoryId: 12,
     stock: 10,
   },
   {
@@ -49,7 +49,7 @@ const productsToPreLoad: IProduct[] = [
       "Stay connected and healthy with the Apple Watch Series 6: track your workouts, monitor your health, and stay in touch with the people and information you care about most. Experience the future of health and wellness with the Apple Watch Series 6.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/AppleWatchSeries6.jpeg?updatedAt=1755005060979",
-    categoryId: 4,
+    categoryId: 18,
     stock: 10,
   },
   {
@@ -59,7 +59,7 @@ const productsToPreLoad: IProduct[] = [
       "Immerse yourself in sound with the AirPods Pro: active noise cancellation, transparency mode, and customizable fit make the AirPods Pro the perfect companion for music, calls, and everything in between. Elevate your audio experience with the AirPods Pro.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/airpodspro2.jpeg?updatedAt=1755005057500",
-    categoryId: 5,
+    categoryId: 13,
     stock: 10,
   },
   {
@@ -69,7 +69,7 @@ const productsToPreLoad: IProduct[] = [
       "Elevate your home audio experience with the HomePod mini: immersive sound, intelligent assistant, and smart home hub make the HomePod mini the perfect addition to your home. Enjoy a world of music, news, and more with the HomePod mini.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/homepod-mini.jpg?updatedAt=1755005058515",
-    categoryId: 6,
+    categoryId: 13,
     stock: 10,
   },
   {
@@ -79,7 +79,7 @@ const productsToPreLoad: IProduct[] = [
       "Step into the future with Apple Vision Pro: augmented reality redefined with immersive visuals and seamless integration into your digital life.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/applevisionpro.jpg?updatedAt=1755005060927",
-    categoryId: 9,
+    categoryId: 18,
     stock: 5,
   },
   {
@@ -89,7 +89,7 @@ const productsToPreLoad: IProduct[] = [
       "Bring cinema to your home with Apple TV 4K: stunning picture quality, Dolby Atmos sound, and access to your favorite streaming apps.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/appletv4k.webp?updatedAt=1755005060606",
-    categoryId: 9,
+    categoryId: 16,
     stock: 4,
   },
   {
@@ -99,7 +99,7 @@ const productsToPreLoad: IProduct[] = [
       "Unleash creative potential with Mac Studio: blazing-fast M-series chips, high performance, and compact power for pros.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/macstudio.jpg?updatedAt=1755005057802",
-    categoryId: 6,
+    categoryId: 11,
     stock: 7,
   },
   {
@@ -109,7 +109,7 @@ const productsToPreLoad: IProduct[] = [
       "Portability meets performance with the iPad Mini: small size, A15 chip, and Apple Pencil support for powerful productivity anywhere.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/ipadmini.jpeg?updatedAt=1755005057796",
-    categoryId: 3,
+    categoryId: 12,
     stock: 4,
   },
   {
@@ -119,7 +119,7 @@ const productsToPreLoad: IProduct[] = [
       "Designed for adventure: Apple Watch Ultra is rugged, capable, and built to meet the demands of endurance athletes and explorers.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/AppleWatchUltra.jpg?updatedAt=1755005057570",
-    categoryId: 9,
+    categoryId: 18,
     stock: 8,
   },
   {
@@ -129,7 +129,7 @@ const productsToPreLoad: IProduct[] = [
       "High-fidelity audio meets over-ear luxury: AirPods Max delivers immersive sound, active noise cancellation, and premium design.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/airpodsmax.webp?updatedAt=1755005058166",
-    categoryId: 4,
+    categoryId: 13,
     stock: 10,
   },
   {
@@ -139,7 +139,7 @@ const productsToPreLoad: IProduct[] = [
       "Reimagined sound for your home with HomePod 2nd Gen: intelligent assistant, spatial awareness, and deep bass in a sleek design.",
     image:
       "https://ik.imagekit.io/p2ho5d9bi/E-Commerce/AppleHomePod2ndGen.jpg?updatedAt=1755005058034",
-    categoryId: 9,
+    categoryId: 13,
     stock: 5,
   },
 ];
