5d692afb03d005db73df5d87bc3e448661c13bc9
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2025-08-13 15:10:31 +0200

    Modified next.config with imagekit's url

--- front/next.config.ts
+++ front/next.config.ts
@@ -9,48 +9,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.apple.com",
-      },
-      {
-        protocol: "https",
-        hostname: "arrichetta.com.ar",
-      },
-      {
-        protocol: "https",
-        hostname: "acdn-us.mitiendanube.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.megatone.net",
-      },
-      {
-        protocol: "https",
-        hostname: "aihome.com.my",
-      },
-      {
-        protocol: "https",
-        hostname: "png.pngtree.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.samsung.com",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "store.google.com",
-      },
-      {
-        protocol: "https",
-        hostname: "m.media-amazon.com",
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "media.istockphoto.com",
+        hostname: "ik.imagekit.io",
       },
     ],
   },
